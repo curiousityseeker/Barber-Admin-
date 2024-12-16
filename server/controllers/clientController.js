a45427ab6a25752d32797e8e949aa2d0319659b6
Amaan <ashadamaan2@gmail.com>
2024-12-16 17:35:30 +0530

    removed .env

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -1,40 +1,40 @@
 import client from '../database/db.js';
-// const addClient = async (req, res) => {
-//     try {
-//       await client.query("set search_path to 'admin'");
+const addClient = async (req, res) => {
+    try {
+      await client.query("set search_path to 'admin'");
 
-//       const { client_id, name, appointment_time, barber_id, status } = req.body;
+      const { client_id, name, appointment_time, barber_id, status } = req.body;
 
-//       const c_id = parseInt(client_id);
-//       const b_id = parseInt(barber_id);
+      const c_id = parseInt(client_id);
+      const b_id = parseInt(barber_id);
 
-//       if (isNaN(c_id) || isNaN(b_id)) {
-//         return res
-//           .status(400)
-//           .json({ error: "Invalid client_id or barber_id "  ,value: c_id });
-//       }
+      if (isNaN(c_id) || isNaN(b_id)) {
+        return res
+          .status(400)
+          .json({ error: "Invalid client_id or barber_id "  ,value: c_id });
+      }
 
-//       const a_time = new Date(appointment_time);
+      const a_time = new Date(appointment_time);
 
-//       // Ensure the appointment_time is a valid date
-//       if (isNaN(a_time.getTime())) {
-//         return res.status(400).json({ error: "Invalid appointment_time" });
-//       }
-//       const result = await client.query(
-//         "INSERT INTO clients(client_id,client_name, appointment_time, barber_id, status) VALUES($1, $2, $3, $4,$5) RETURNING *",
-//         [c_id, name, a_time, b_id, status]
-//       );
-//       res.status(200).json(result.rows);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// };
+      // Ensure the appointment_time is a valid date
+      if (isNaN(a_time.getTime())) {
+        return res.status(400).json({ error: "Invalid appointment_time" });
+      }
+      const result = await client.query(
+        "INSERT INTO clients(client_id,client_name, appointment_time, barber_id, status) VALUES($1, $2, $3, $4,$5) RETURNING *",
+        [c_id, name, a_time, b_id, status]
+      );
+      res.status(200).json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
 
 const fetchClients = async (req, res) => {
     try {
         await client.query("set search_path to 'admin'");
         const result = await client.query('SELECT * FROM clients');
-        res.status(200).json(result.rows);
+        res.status(200).json(result.rows);  
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -53,4 +53,4 @@ const updateAppointmentStatus = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
-export { fetchClients ,updateAppointmentStatus};
+export { addClient,fetchClients ,updateAppointmentStatus};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
-import { fetchClients, updateAppointmentStatus} from "../controllers/clientController.js";
+import { addClient,fetchClients, updateAppointmentStatus} from "../controllers/clientController.js";
 import { fetchBarber } from "../controllers/barberController.js";
 const router = express.Router();
 
 router.get('/allClients',fetchClients);
 router.get("/allBarber/:id?",fetchBarber);
+router.post("/addClient", addClient);
 router.put("/updateStatus", updateAppointmentStatus);
 export default router;
